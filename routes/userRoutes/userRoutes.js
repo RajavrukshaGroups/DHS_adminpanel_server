55062efa9cb35006d96d85f4fabd2b04a7dbc1ee
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-05-05 17:37:04 +0530

    completed authetication

--- routes/userRoutes/userRoutes.js
+++ routes/userRoutes/userRoutes.js
@@ -1,4 +1,7 @@
 import express from "express";
+import authController from "../../controller/authController.js";
 const router = express.Router();
+
+router.post('/adminLogin',authController.adminLogin)
 
 export default router;
