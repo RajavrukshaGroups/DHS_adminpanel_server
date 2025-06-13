d9e7dcd02708e7e276da2e17ee6949de143fbd8e
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-06-13 17:07:34 +0530

    added application for membership

---
+++ utils/generatePDF.js
@@ -0,0 +1,20 @@
+// utils/generatePDF.js
+import puppeteer from 'puppeteer';
+export const generatePDFBuffer = async (htmlContent) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    printBackground: true,
+  });
+
+  await browser.close();
+  return pdfBuffer;
+};
+
+// module.exports = generatePDFBuffer;
