99e00c09a51ead1e63ccf0b0784091e10d96ed5f
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-09-25 18:34:40 +0530

    updation

--- index.js
+++ index.js
@@ -31,13 +31,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const allowedOrigins = [
-  "https://defencehousingsociety.com",
-  "https://testingadminpanel.defencehousingsociety.com",
-  "https://defencehabitat-tapasihalli.defencehousingsociety.com",
-];
+// const allowedOrigins = [
+//   "https://defencehousingsociety.com",
+//   "https://testingadminpanel.defencehousingsociety.com",
+//   "https://defencehabitat-tapasihalli.defencehousingsociety.com",
+// ];
 
-// const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+
 app.use(
   cors({
     origin: function (origin, callback) {
